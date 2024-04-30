c6f062861d0e211d6fe2da2098ed737fdba58f81
Joker13721 <168573195+Joker13721@users.noreply.github.com>
2024-04-30 18:12:56 +0200

    Update main-2.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
 # Écrivez votre code ici !
+nom = "Mohamed"
+age = 20
+taille = 1.83
+est_etudiant = True
+print(f"Nom: {nom}")
+print(f"Âge: {age}")
+print(f"Taille: {taille}")
+print(f"Statut: {est_etudiant}")
+
+print(f"Type du nom: {type(nom)}")
+print(f"Type de l'âge: {type(age)}")
+print(f"Type de la taille: {type(taille)}")
+print(f"Type de Statut: {type(est_etudiant)}")
