b40a3de70bf2fd3118093860cd6186f200383224
Joker13721 <168573195+Joker13721@users.noreply.github.com>
2025-08-12 15:31:59 +0200

    Update main.py
    Bonjour, j'espère que vous allez bien!
    
    Voici mon programme sur cet exercice, s'il y a possibilité d'avoir un retour de votre part, j'en serai ravi et vous pourrez me contacter à l'adresse mail suivant : mohamedacamara1@gmail.com.
    
    Je vous prie d'agréer de mes salutations distinguées.
    
    Cordialement 
    
    Camara Mohamed

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,12 @@
 ## Écrivez votre code ici !
+
+# Début du programme
+
+# J'utilise la fonction "print()" pour afficher la phrase "J'apprends Python !"
+
+print("J'apprends Python !")
+
+# Je calcul et affiche le résultat de l'opération suivante : "17 + 35 * 2" 
+print(17 + 35 * 2)
+
+# Fin du programme
