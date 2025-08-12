99f760b626c799f656b8308e71ff62311c5ce07d
Joker13721 <168573195+Joker13721@users.noreply.github.com>
2025-08-12 16:42:57 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+# Exercice : Variables et F-String
+
+# Création des différentes variables
+nom = "Joker"
+age = 21
+
+# Affichage des variables
+# print(f"Je m'appelle {nom} et j'ai {age} ans.")
+
+# Modifications de la variable age
+age = age + 10
+
+# Affichage
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
